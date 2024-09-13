baa3e6057070fb15ea8bb3e369b77c8a27491fcf
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-09-13 22:39:27 +0530

    added a new file

--- array.cpp
+++ array.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n, i;
-    int arr[i] = {100, 220, 400, 300};
-    for (int i = 0; i < n; i++)
+    int arr[] = {100, 220, 400, 300};
+    for (int i = 0; i < 4; i++){
         cout << arr[i] ;
     cout << endl;
+    }
     return 0;   
 }
 
